49265930992aaae54e9d83c5e7e76be61d7c8708
s-gabor <ing.gabor@yahoo.com>
2018-12-17 12:55:56 +0200

    add class names to constatns/update nt name in utils.create_nt, create parse_date parser in utils/remove date parser condition from parse_row

--- constants.py
+++ constants.py
@@ -1,16 +1,28 @@
 # constants.py
 
-from datetime import datetime
+from utils import parse_date
 
+
+# Files
 fname_personal = 'data_files/personal_info.csv'
 fname_employment = 'data_files/employment.csv'
 fname_update_status = 'data_files/update_status.csv'
 fname_vehicles = 'data_files/vehicles.csv'
 fnames = [fname_personal, fname_employment, fname_update_status, fname_vehicles]
 
+
+# Parsers
 date_format = '%Y-%m-%dT%H:%M:%SZ'
 data_type_personal = (str, str, str, str, str)
 data_type_employment = (str, str, str, str)
-data_type_update_status = (str, 'date', 'date')
+data_type_update_status = (str, parse_date, parse_date)
 data_type_vehicles = (str, str, str, int)
 data_types = (data_type_personal, data_type_employment, data_type_update_status, data_type_vehicles)
+
+
+# Named tuple names
+personal_class_name = 'Personal'
+employment_class_name = 'Employment'
+update_status_class_name = 'UpdateStatus'
+vehicles_class_name = 'Vehicle'
+class_names = (personal_class_name, employment_class_name, update_status_class_name, vehicles_class_name)
